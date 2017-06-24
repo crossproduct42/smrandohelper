47a546b9cd21136171c15728240f8121ab306445
Roger Norling <roger.norling.se@gmail.com>
2017-06-24 03:59:01 +0200

    String literal style
    This is an opinionated commit. I have two reasons for preferring single quote string literals:
    1. Style; the single quotes is a "smaller" character, making it less busy on the screen.
    2. Markup convention; By using single quotes, writing any markup as strings let us use double quote around attribute values without the need of character escaping.

--- script/items.js
+++ script/items.js
@@ -11,7 +11,7 @@
     window.ammo = {
         missile: {
             value: 0,
-            icon: "items/missile.png",
+            icon: 'items/missile.png',
             w: 96,
             h: 64,
             x: 0,
@@ -20,7 +20,7 @@
 
         supermissile: {
             value: 0,
-            icon: "items/super.png",
+            icon: 'items/super.png',
             w: 64,
             h: 64,
             x: 130,
@@ -29,7 +29,7 @@
 
         powerbomb: {
             value: 0,
-            icon: "items/pbomb.png",
+            icon: 'items/pbomb.png',
             w: 64,
             h: 64,
             x: 224,
@@ -40,98 +40,98 @@
     window.items = {
         charge: {
             value: false,
-            icon: "items/charge.png",
+            icon: 'items/charge.png',
             x: 8,
             y: 97
         },
 
         ice: {
             value: false,
-            icon: "items/ice.png",
+            icon: 'items/ice.png',
             x: 69,
             y: 97
         },
 
         wave: {
             value: false,
-            icon: "items/wave.png",
+            icon: 'items/wave.png',
             x: 130,
             y: 97
         },
 
         spazer: {
             value: false,
-            icon: "items/spazer.png",
+            icon: 'items/spazer.png',
             x: 191,
             y: 97
         },
 
         plasma: {
             value: false,
-            icon: "items/plasma.png",
+            icon: 'items/plasma.png',
             x: 252,
             y: 97
         },
 
         varia: {
             value: false,
-            icon: "items/varia.png",
+            icon: 'items/varia.png',
             x: 130,
             y: 149
         },
 
         gravity: {
             value: false,
-            icon: "items/gravity.png",
+            icon: 'items/gravity.png',
             x: 192,
             y: 149
         },
 
         morph: {
             value: false,
-            icon: "items/morph.png",
+            icon: 'items/morph.png',
             x: 252,
             y: 146
         },
 
         bombs: {
             value: false,
-            icon: "items/bomb.png",
+            icon: 'items/bomb.png',
             x: 130,
             y: 201
         },
 
         springball: {
             value: false,
-            icon: "items/springball.png",
+            icon: 'items/springball.png',
             x: 190,
             y: 201
         },
 
         screw: {
             value: false,
-            icon: "items/screw.png",
+            icon: 'items/screw.png',
             x: 250,
             y: 198
         },
 
         hijump: {
             value: false,
-            icon: "items/hijump.png",
+            icon: 'items/hijump.png',
             x: 127,
             y: 253
         },
 
         space: {
             value: false,
-            icon: "items/space.png",
+            icon: 'items/space.png',
             x: 190,
             y: 253
         },
 
         speed: {
             value: false,
-            icon: "items/speed.png",
+            icon: 'items/speed.png',
             x: 250,
             y: 251
         }
